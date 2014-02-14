1dadc730d314216753862d93043c41fcc996e49d
vagrant <vagrant@precise32.(none)>
2014-02-14 20:53:02 +0000

    finished dynamic loading

--- public/js/login-listen.js
+++ public/js/login-listen.js
@@ -35,24 +35,44 @@ function gotEvents(result){
 	}
 
 	milestones.sort(dateComp);
-	milestones.reverse();
-
 
 	for(var i = 0; i < milestones.length ; ){
-		var date = milestonelist[i].actualDate;
+		var date = milestones[i].actualDate;
 		var dayMilestones = new Array();
-		while(milestones[i].actualDate == date){
+		while(i < milestones.length && milestones[i].actualDate.getTime() == date.getTime()){
 			dayMilestones.push(milestones[i]);
 			i++;
 		}
 
+		var string = '<h2 class="date">'+ date.toDateString() + ' </h2>'
+		for(var j = 0; j < dayMilestones.length; ++j){
+			if(dayMilestones[j].actualDate > Date.now()){
+				string+= '<div class = "event event-incomplete">';
+			}
+			else{
+				string+= '<div class = "event">';
+				string+= '<p class="event-complete-time">' + 'Completed at ' + dayMilestones[j].time + '</p>';
+			}
+			string+=
+                '<p class="milestone-progress">' +
+                	(dayMilestones[j].milestoneNum+1) + '/'+goals[dayMilestones[j].goalNum]['milestones'].length+
+                '</p><br />' +
+    			'<p class="event-description">'+ dayMilestones[j].name + '</p>' +
+    			'</div>';
+		}
+		string = '<div class = "day">' + string  + '</div>';
+		if(i == milestones.length)
+			string += '<div class="event event-complete event-add">'+
+                	'<p class="event-description"><a href="/add-milestone" class="add-milestone">Add Milestone</a></p>'+
+                '</div>';
+   		timeline.append(string);
 	}
 
 	console.log(milestones);
 }
 
 function dateComp(a,b){
-	return new Date(b.actualDate) - new Date(a.actualDate);
+	return new Date(a.actualDate)-new Date(b.actualDate);
 }
 
 function loginFunction(e){
